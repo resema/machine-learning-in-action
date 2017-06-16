7e6bbacf931815c1be032381a0844ea1a68bd95e
Renato Semadeni <renato.semadeni@gmail.com>
2017-06-16 16:34:37 +0200

    simple svm started

---
+++ ch06/svmMLiA.py
@@ -0,0 +1,81 @@
+from numpy import *
+
+def loadDataSet(fileName):
+  dataMat = []; labelMat = []
+  fr = open(fileName)
+  for line in fr.readlines():
+    lineArr = line.strip().split('\t')
+    dataMat.append([float(lineArr[0]), float(lineArr[1])])
+    labelMat.append(float(lineArr[2]))
+  return dataMat, labelMat
+  
+def selectJrand(i, m):
+  j = i
+  while (j==i):
+    j = int(random.uniform(0,m))
+  return j
+  
+def clipAlpha(aj, H, L):
+  if aj > H:
+    aj = H
+  if L > aj:
+    aj = L
+  return aj
+  
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+  dataMatrix = mat(dataMatIn)
+  labelMat = mat(classLabels).transpose()
+  b = 0; m,n = shape(dataMatrix)
+  alphas = mat(zeros((m,1)))
+  iter = 0
+  while (iter < maxIter):
+    alphaPairsChanged = 0
+    for i in range(m):
+      fXi = float(multiply(alphas, labelMat).T * (dataMatrix*dataMatrix[i,:].T)) + b
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
