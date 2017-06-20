bdc3a458c6ad17c6697b06b34c7bf480ff87fc4d
Renato Semadeni <renato.semadeni@gmail.com>
2017-06-20 16:38:52 +0200

    Full platt smo started

--- ch06/svmMLiA.py
+++ ch06/svmMLiA.py
@@ -1,5 +1,15 @@
+'''
+Created on Jun 16 2017
+Logistic Regression Working Module
+@author: resema
+'''
+
 from numpy import *
 
+'''
+simple minimize optimization approch
+'''
+    
 def loadDataSet(fileName):
   dataMat = []; labelMat = []
   fr = open(fileName)
@@ -31,8 +41,181 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
   while (iter < maxIter):
     alphaPairsChanged = 0
     for i in range(m):
-      fXi = float(multiply(alphas, labelMat).T * (dataMatrix*dataMatrix[i,:].T)) + b
+      # prediction of the class
+      fXi = float(multiply(alphas, labelMat).T*(dataMatrix*dataMatrix[i,:].T)) + b
+      # error Ei based on the prediction and the real class of this instance
+      Ei = fXi - float(labelMat[i])
+      # 1: enter optimization if alphas can be changed
+      if ((labelMat[i]*Ei < -toler) and (alphas[i] < C)) or \
+         ((labelMat[i]*Ei > toler) and (alphas[i] > 0)):
+        # 2: randomly select second alpha
+        j = selectJrand(i,m)
+        fXj = float(multiply(alphas, labelMat).T*(dataMatrix*dataMatrix[j,:].T)) + b
+        Ej = fXj - float(labelMat[j])
+        alphaIold = alphas[i].copy()
+        alphaJold = alphas[j].copy()
+        # 3: guarantee alphas stay between 0 and C
+        if (labelMat[i] != labelMat[j]):
+          L = max(0, alphas[j] - alphas[i])
+          H = min(C, C + alphas[j] - alphas[i])
+        else:
+          L = max(0, alphas[j] + alphas[i] - C)
+          H = min(C, alphas[j] + alphas[i])
+        if L==H:
+          print "L==H"
+          continue
+        eta = 2.0 * dataMatrix[i,:]*dataMatrix[j,:].T \
+                  - dataMatrix[i,:]*dataMatrix[i,:].T \
+                  - dataMatrix[j,:]*dataMatrix[j,:].T
+        if eta >= 0:
+          print "eta>=0"
+          continue
+        alphas[j] -= labelMat[j]*(Ei - Ej)/eta
+        alphas[j] = clipAlpha(alphas[j], H, L)
+        if (abs(alphas[j] - alphaJold) < 0.00001):
+          print "j not moving enough"
+          continue
+        # 4: update i by same amount as j in opposite direction
+        alphas[i] += labelMat[j]*labelMat[i]*(alphaJold - alphas[j])
+        # 5: set the constant term
+        b1 = b - Ei - labelMat[i]*(alphas[i]-alphaIold)*dataMatrix[i,:]*dataMatrix[i,:].T \
+                    - labelMat[j]*(alphas[j]-alphaJold)*dataMatrix[i,:]*dataMatrix[j,:].T
+        b2 = b - Ej - labelMat[i]*(alphas[i]-alphaIold)*dataMatrix[i,:]*dataMatrix[j,:].T \
+                    - labelMat[j]*(alphas[j]-alphaJold)*dataMatrix[j,:]*dataMatrix[j,:].T
+        if (0 < alphas[i]) and (C > alphas[i]):
+          b = b1
+        elif (0 < alphas[j]) and (C > alphas[j]):
+          b = b2
+        else:
+          b = (b1 + b2)/2.0
+        alphaPairsChanged += 1
+        print "iter: %d i:%d, pairs changed %d" % (iter, i, alphaPairsChanged)
+    if (alphaPairsChanged == 0):
+      iter += 1
+    else:
+      iter = 0
+    print "iteration number: %d" % iter
+  return b,alphas
   
+'''
+full platt SMO approach
+'''
+  
+class optStruct:
+  def __init__(self,dataMatIn, classLabels, C, toler):
+    self.X = dataMatIn
+    self.labelMat = classLabels
+    self.C = C
+    self.tol = toler
+    self.m = shape(dataMatIn)[0]
+    self.alphas = mat(zeros((self.m,1)))
+    self.b = 0
+    # 1: error cache
+    self.eCache = mat(zeros((self.m,2)))
+  
+def calcEk(oS,k):
+  fXk = float(multiply(oS.alphas,oS.labelMat).T*(oS.X*oS.X[k,:].T)) + oS.b
+  Ek = fXk - float(oS.labelMat[k])
+  return Ek
+  
+def selectJ(i, oS, Ei):
+  # 2: inner loop heuristic
+  maxK = -1; maxDeltaE = 0; Ej = 0
+  oS.eCache[i] = [1,Ei]
+  validEcacheList = nonzero(oS.eCache[:,0].A)[0]
+  if (len(validEcacheList)) > 1:
+    for k in validEcacheList:
+      if k==i:
+        continue
+      Ek = calcEk(oS, k)
+      deltaE = abs(Ei - Ek)
+      # 3: choose j for maximum step size
+      if (deltaE > maxDeltaE):
+        maxK = k; maxDeltaE = deltaE; Ej = Ek
+    return maxK, Ej
+  else:
+    j = selectJrand(i, oS.m)
+    Ej = calcEk(oS, j)
+  return j, Ej
+  
+def updateEk(oS, k):
+  Ek = calcEk(oS, k)
+  oS.eCache[k] = [1, Ek]
+  
+def innerL(i, oS):
+  Ei = calcEk(oS, i)
+  if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or \
+     ((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
+    # 1: second-choice heuristic
+    j, Ej = selectJ(i, oS, Ei)
+    alphaIold = oS.alphas[i].copy(); alphaJold = oS.alphas[j].copy()
+    if (oS.labelMat[i] != oS.labelMat[j]):
+      L = max(0, oS.alphas[j] - oS.alphas[i])
+      H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+    else:
+      L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+      H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+    if L==H:
+      print "L==H"
+      return 0
+    eta = 2.0 * oS.X[i,:]*oS.X[j,:].T - oS.X[i,:]*oS.X[i,:].T - oS.X[j,:]*oS.X[j,:].T
+    if eta >= 0:
+      print "eta>=0"
+      return 0
+    oS.alphas[j] -= oS.labelMat[j]*(Ei - Ej)/eta
+    oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
+    # 2: updates Ecache
+    updateEk(oS, j)
+    if (abs(oS.alphas[j] - alphaJold) < 0.00001):
+      print "j not moving enough"
+      return 0
+    oS.alphas[i] += oS.labelMat[j]*oS.labelMat[i]*(alphaJold - oS.alphas[j])
+    # 2: update Ecache
+    updateEk(oS, i)
+    b1 = oS.b - Ei - oS.labelMat[i]*(oS.alphas[i]-alphaIold)* \
+         oS.X[i,:]*oS.X[i,:].T - oS.labelMat[j]* \
+         (oS.alphas[j]-alphaJold)*oS.X[i,:]*oS.X[j,:].T
+    b2 = oS.b - Ej - oS.labelMat[i]*(oS.alphas[i]-alphaIold)* \
+         oS.X[i,:]*oS.X[j,:].T - oS.labelMat[j]* \
+         (oS.alphas[j]-alphaJold)*oS.X[j,:]*oS.X[j,:].T
+    if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]):
+      oS.b = b1
+    elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]):
+      oS.b = b2
+    else:
+      oS.b = (b1 + b2)/2.0
+    return 1
+  else:
+  return 0
+  
+def smoP(dataMatIn, classLabels, C, toler, maxIter, kTup=('lin', 0)):
+  oS = optStruc(mat(dataMatIn), mat(classLabels).transpose(), C, toler)
+  iter = 0
+  entireSet = True; alphaPairsChanged = 0
+  while (iter < maxIter) and ((alphaPairsChanged > 0) or (entireSet)):
+    alphaPairsChanged = 0
+    if entireSet:
+      for i in range(oS.m):
+        alphaPairsChanged += innerL(i, oS)
+      print "fullset, iter: %d i:%d, pairs chagned %d" % (iter, i, alphaPairsChanged)
+      iter += 1
+    else:
+      nonBoundIs = nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
+      for i in nonBoundIs:
+        alphaPairsChanged += innerL(i, oS)
+        print "non-bound, iter: %d i:%d, pairs changed %d" % (iter, i, alphaPairsChanged)
+      iter += 1
+    if entireSet:
+      entireSet = False
+    elif (alphaPairsChanged == 0):
+      entireSet = True
+    print "iteration number %d" % iter
+  return oS.b, oS.alphas
+      
+  
+
+  
+
 
 
 
