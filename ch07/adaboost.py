0ed0eba8a0b5291878a06cbbe602a9b12b803c21
Renato Semadeni <renato.semadeni@gmail.com>
2017-06-22 17:29:32 +0200

    initial commit for adaBoost

---
+++ ch07/adaboost.py
@@ -0,0 +1,57 @@
+'''
+Created on Jun 22 2017
+AdaBoost example
+@author: resema
+'''
+from numpy import *
+
+def loadSimpData():
+  datMat = matrix([[1., 2.1],
+                   [2., 1.1],
+                   [1.3, 1.],
+                   [1., 1.],
+                   [2., 1.]])
+  classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+  return datMat, classLabels
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
