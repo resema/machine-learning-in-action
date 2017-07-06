8f321235db766571066999d2cfe8514fdc5352c3
Renato Semadeni <renato.semadeni@gmail.com>
2017-07-06 16:32:51 +0200

    google example not working due to deprecated shopping api

--- ch08/regression.py
+++ ch08/regression.py
@@ -1,6 +1,10 @@
 '''
 Created on Jun 26 2017
 Linear Regression example
+
+  Characteristics:  process of predicting a target value similar to classification
+                    variable forcast versus discrete in classification
+
 @author: resema
 '''
 from numpy import *
@@ -125,6 +129,84 @@ def stageWise(xArr, yArr, eps=0.01, numIt=100):
     returnMat = zeros((numIt,n)) 
     returnMat[i,:] = ws.T
   return returnMat
+  
+'''
+shopping inforamtion retrieval function
+
+  using google shopping search api
+'''
+from time import sleep
+import json
+import urllib2
+
+def searchForSet(retX, retY, setNum, yr, numPce, origPrc):
+  sleep(10)
+  myAPIstr = 'AIzaSyCynWtnF3PwMxSyU2HhT2jOzqTKbEgRH4Q'
+  searchURL = 'https://www.googleapis.com/shopping/search/v1/public/products?key=%s&country=US&q=lego+%d&alt=json' \
+                % (myAPIstr, setNum)
+  pg = urllib2.urlopen(searchURL)
+  retDict = json.loads(pg.read())
+  # from api content of json package
+  for i in range(len(retDict['items'])):
+    try:
+      currItem = retDict['items'][i]
+      if currItem['product']['condition'] == 'new':
+        newFlag = 1
+      else:
+        newFlag = 0
+        listOfInv = currItem['product']['inventories']
+        for item in listOfInv:
+          sellingPrice = item['price']
+          if sellingPrice > origPrc * 0.5:
+            print "%d\t%d\t%d\t%f\t%f" % (yr, numPce, newflag, origPrc, sellingPrice)
+            retX.append([yr, numPce, newFlag, origPrc])
+            retY.append(sellingPrice)
+    except:
+      print 'problem with %d' % i
+      
+def setDataCollect(retX, retY):
+  searchForSet(retX, retY, 8288, 2006, 800, 49.99)
+  searchForSet(retX, retY, 10030, 2002, 3096, 169.99)
+  searchForSet(retX, retY, 10179, 2007, 5195, 499.99)
+  searchForSet(retX, retY, 10181, 2007, 3428, 199.99)
+  searchForSet(retX, retY, 10189, 2008, 5922, 299.99)
+  searchForSet(retX, retY, 10196, 2009, 3263, 249.99)
+  
+def crossValidation(xArr, yArr, numVal=10):
+  m = len(yArr)
+  indexList = range(m)
+  errorMat = zeros((numVal,30))
+  for i in range(numVal):
+    # 1: create training and test containers
+    trainX = []; trainY = []
+    testX = []; testY = []
+    random.shuffle(indexList)
+    for j in range(m):
+      if j < m*0.9:
+        # 2: split data into test and training sets
+        trainX.append(xArr(indexList[j]))
+        trainY.append(yArr(indexList[j]))
+      else:
+        testX.append(xArr(indexList[j]))
+        testY.append(yArr(indexList[j]))
+    wMat = ridgeTEst(trainX, trainY)
+    for k in range(30):
+      # 3: regularize test with training params
+      matTestX = mat(testX); matTrainX = mat(trainX)
+      meanTrain = mean(matTrainX,0)
+      varTrain = var(matTrainX,0)
+      matTestX = (matTestX-meanTrain)/varTrain
+      yEst = matTestX * mat(wMat[k,:].T + mean(trainY)
+      errorMat[i,k] = rssError(yEst.T.A, array(testY))
+  meanErrors = mean(errorMat,0)
+  minMean = float(min(meanErrors))
+  bestWeights = wMat[nonzero(meanErrors==minMean)]
+  xMat = mat(xArr); yMat = mat(yArr).T
+  meanX = mean(xMat,0); varX = var(xMat,0)
+  # 4: undo regularization
+  unReg = bestWeights/varX
+  print "the best model from Ridge Regression is:\n", unReg
+  print "with constant term:", -1*sum(multiply(meanX, unReg)) + mean(yMat)
 
 
 
