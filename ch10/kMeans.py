758174a4f38c5c6ef9eb1306d2f81767f6952828
Renato Semadeni <renato.semadeni@gmail.com>
2017-07-13 06:40:21 +0200

    bisecting k-means started

--- ch10/kMeans.py
+++ ch10/kMeans.py
@@ -54,7 +54,73 @@ def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
       centroids[cent,:] = mean(ptsInClust, axis=0)
   return centroids, clusterAssment
 
-
+# bisecting k-means clustering algorithm
+def biKmeans(dataSet, k, distMeas=distEclud):
+  m = shape(dataSet)[0]
+  clusterAssment = mat(zeros((m,2)))
+  # initially create one cluster
+  centroid0 = mean(dataSet, axis=0).tolist()[0]
+  centList = [centroid0]
+  for j in range(m):
+    clusterAssment[j,1] = distMeas(mat(centroid0), dataSet[j,:])**2
+  while(len(centList) < k):
+    lowestSSE = inf   # SSE: sum of squared errors
+    for i in range(len(centList)):
+      # try splitting every cluster
+      ptsInCurrCluster = dataSet[nonzero(clusterAssment[:,0].A==i)[0],:]
+      centroidMat, splitCustAss = kMeans(ptsInCurrCluster, 2, disMeas)
+      sseSplit = sum(splitClustAss[:,1])
+      sseNotSplit = sum(clusterAssment[nonzero(clusterAssment[:,0].A!=i)[0],1])
+      print "sseSplit, and notSplit: ", sseSplit, sseNotSplit
+      if(sseSplit + sseNotSplit) < lowestSSE:
+        bestCentToSplit = i
+        bestNewCents = centroidMat
+        bestClusAss = splitClustAss.copy()
+        lowestSSE = sseSplit + sseNotSplit
+    # update the cluster assignments
+    bestClustAss[nonzero(bestClustAss[:,0].A == 1)[0],0] = len(centList)
+    bestClussAss[nonzero(bestClustAss[:,0].A == 0)[0],0] = bestCentToSplit
+    print 'the bestCentToSplit is: ', bestCentToSplit
+    print 'the len of bestClustAss is: ', len(bestClustAss)
+    centList[bestCentToSplit] = bestNewCents[0,:]
+    centList.append(bestNewCents[1,:])
+    clusterAssment[nonzero(clusterAssment[:,0].A == bestCentToSplit)[0],:] = bestClustAss
+  return mat(centList), clusterAssment
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 
 
