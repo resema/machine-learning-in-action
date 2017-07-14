c29b58d78df0a11e3da7166526aa04e89c5a6e68
Renato Semadeni <renato.semadeni@gmail.com>
2017-07-14 16:21:24 +0200

    centroids not calculated correctly, problems with array/sequence

--- ch10/kMeans.py
+++ ch10/kMeans.py
@@ -120,6 +120,39 @@ def massPlaceFind(fileName):
     sleep(1)
   fw.close()
   
+def distSLC(vecA, vecB):
+  a = sin(vecA[0,1]*pi/180) * sin(vecB[0,1]*pi/180)
+  b = cos(vecA[0,1]*pi/180) * cos(vecB[0,1]*pi/180) * cos(pi * (vecB[0,0]-vecA[0,0]) / 180)
+  return arccos(a + b)*6371.0
+  
+import matplotlib
+import matplotlib.pyplot as plt
+def clusterClubs(numClust=5):
+  datList = []
+  for line in open('places.txt').readlines():
+    lineArr = line.split('\t')
+    datList.append([float(lineArr[4]), float(lineArr[3])])
+  datMat = mat(datList)
+  myCentroids, clustAssing = biKmeans(datMat, numClust, distMeas=distSLC) # why assignment in function passing
+  fig = plt.figure()
+  rect=[0.1,0.1,0.8,0.8]
+  scatterMarkers = ['s','o','^','8','p','d','v','h','>','<']
+  axprops = dict(xticks=[], yticks=[])
+  ax0 = fig.add_axes(rect, label='ax0', **axprops)
+  # 1: create matrix from image
+  imgP = plt.imread('Portland.png')
+  ax0.imshow(imgP)
+  ax1 = fig.add_axes(rect, label='ax1', frameon=False)
+  for i in range(numClust):
+    ptsInCurrCluster = datMat[nonzero(clustAssing[:,0].A==i)[0],:]
+    markerStyle = scatterMarkers[i % len(scatterMarkers)]
+    ax1.scatter(ptsInCurrCluster[:,0].flatten().A[0], \
+                ptsInCurrCluster[:,1].flatten().A[0], \
+                marker=markerStyle, s=90)
+  ax1.scatter(myCentroids[:,0].flatten().A[0], \
+              myCentroids[:,1].flatten().A[0], marker='+', s=300)
+  plt.show()
+  
   
   
   
