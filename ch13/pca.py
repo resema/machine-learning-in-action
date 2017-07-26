a34510e30f81b6602c867b1942788b1c182eda56
Renato Semadeni <renato.semadeni@gmail.com>
2017-07-26 06:32:29 +0200

    pca working with simple data

--- ch13/pca.py
+++ ch13/pca.py
@@ -1,6 +1,71 @@
 '''
 Created on 26 July 2017
-
-
+Method for dimensionality reduction
+  Principal component reduction (PCA)
+    where is the majority of the variance contained?
+  Factor analysis
+    data is assumed to be a linear combination of latent variable and some noise
+  Independent component analysis (ICA)
+    similar to PCA, but sources are statically independent, which assumes data is uncorrelated
 @autor: resema
 '''
+
+from numpy import *
+
+def loadDataSet(fileName, delim='\t'):
+  fr = open(fileName)
+  stringArr = [line.strip().split(delim) for line in fr.readlines()]
+  datArr = [map(float, line) for line in stringArr]
+  return mat(datArr)
+  
+def pca(dataMat, topNfeat=9999999):
+  meanVals = mean(dataMat, axis=0)
+  # 1: remove mean
+  meanRemoved = dataMat - meanVals
+  covMat = cov(meanRemoved, rowvar=0)
+  eigVals, eigVects = linalg.eig(mat(covMat))
+  eigValInd = argsort(eigVals)
+  # 2: sort top N smallest to largest
+  eigValInd = eigValInd[:-(topNfeat+1):-1]
+  redEigVects = eigVects[:,eigValInd]
+  # 3: transform data into new dimension
+  lowDDataMat = meanRemoved * redEigVects
+  reconMat = (lowDDataMat * redEigVects.T) + meanVals
+  return lowDDataMat, reconMat
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
